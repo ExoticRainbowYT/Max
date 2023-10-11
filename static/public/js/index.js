62501e53ea31ce48b9b20576b616bb4edeb572f6
ExoticRainbowYT <71150874+ExoticRainbowYT@users.noreply.github.com>
2023-10-11 18:45:50 +0000

    Update index.js

--- static/public/js/index.js
+++ static/public/js/index.js
@@ -65,21 +65,64 @@ function isUrl(val = "") {
   
 
 // open url function
-function openURL(url) {
-    window.navigator.serviceWorker
-    .register("./uv.js", {
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  window.navigator.serviceWorker
+    .register("./sw.js", {
       scope: __uv$config.prefix,
     })
     .then(() => {
-      if (!isUrl(url)) url = getSearchEngineURL() + url;
+      let url = input.value.trim();
+      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
       else if (!(url.startsWith("https://") || url.startsWith("http://")))
         url = "http://" + url;
+      sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+      location.href = "go";
+    });
+});
 
-      if (getAboutBlank() === 'on') {
-        openAboutBlank(window.location.href.slice(0, -1) + __uv$config.prefix + __uv$config.encodeUrl(url));
-      } else {
+
+function go(value) {
+    let iframe = document.querySelector(".iframe.active");
+    window.navigator.serviceWorker
+      .register("./sw.js", {
+        scope: __uv$config.prefix,
+      })
+      .then(() => {
+        let url = value.trim();
+        if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+        else if (!(url.startsWith("https://") || url.startsWith("http://")))
+          url = "https://" + url;
+        //pass the encoded url to the second page
+        sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+        location.href = "go";
+      });
+  }
+  
+  function blank(value) {
+    let iframe = document.querySelector(".iframe.active");
+    window.navigator.serviceWorker
+      .register("./sw.js", {
+        scope: __uv$config.prefix,
+      })
+      .then(() => {
+        let url = value.trim();
+        if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+        else if (!(url.startsWith("https://") || url.startsWith("http://")))
+          url = "https://" + url;
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-      }
+      });
+  }
+  
+  function isUrl(val = "") {
+    if (
+      /^http(s?):\/\//.test(val) ||
+      (val.includes(".") && val.substr(0, 1) !== " ")
+    )
+      return true;
+    return false;
+  }
     });
 };
 
